f3e4d6afbb37727b00c5638ec31fe23b40bfe77e
FarjazKashif <farjazkashif01@gmail.com>
2026-07-05 14:53:33 +0500

    pushed: Little business logic

---
+++ apps/api/.wrangler/tmp/bundle-PSbVWd/middleware-insertion-facade.js
@@ -0,0 +1,11 @@
+				import worker, * as OTHER_EXPORTS from "F:\\Farjaz\\Startups\\New folder\\Qulify\\apps\\api\\src\\index.ts";
+				import * as __MIDDLEWARE_0__ from "F:\\Farjaz\\Startups\\New folder\\Qulify\\node_modules\\.pnpm\\wrangler@4.103.0_@cloudflare+workers-types@4.20260621.1\\node_modules\\wrangler\\templates\\middleware\\middleware-ensure-req-body-drained.ts";
+import * as __MIDDLEWARE_1__ from "F:\\Farjaz\\Startups\\New folder\\Qulify\\node_modules\\.pnpm\\wrangler@4.103.0_@cloudflare+workers-types@4.20260621.1\\node_modules\\wrangler\\templates\\middleware\\middleware-miniflare3-json-error.ts";
+
+				export * from "F:\\Farjaz\\Startups\\New folder\\Qulify\\apps\\api\\src\\index.ts";
+				const MIDDLEWARE_TEST_INJECT = "__INJECT_FOR_TESTING_WRANGLER_MIDDLEWARE__";
+				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
+					
+					__MIDDLEWARE_0__.default,__MIDDLEWARE_1__.default
+				]
+				export default worker;
